87d09d1b4fdd86877f8e15e0d1cdb305bfa5fac7
TSOT7 <tommyma1234@gmail.com>
2024-04-05 18:45:45 -0400

    Implemented mouseover fill

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const cdiv = document.querySelector('.container');
 
-// Add 16 divs
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
+  div.setAttribute("id", `theBox${i}`);
   cdiv.appendChild(div);
 }
+
+const boxes = document.querySelectorAll("div.container > div");
+
+boxes.forEach((div) => {
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "black"
+  });
+});
 
 
 
